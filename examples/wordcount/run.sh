e8e34c8d835fb999d70e88d62a3a30c5d8dc5d38
Andryw Marques Ramos <andryw.neo@gmail.com>
2015-02-24 15:21:34 -0300

    run.sh of the WordCount example modified. In previous version there was a bug

--- examples/wordcount/run.sh
+++ examples/wordcount/run.sh
@@ -1,10 +1,22 @@
-outputPath = ""
+#initialize input and output variables
+inputPath=""
+outputPath=""
 
-if [ -z "$2"]
-	then outputPath = "data/wordcount"
-	else outputPath = $2
+#if the inputPath is not passed, uses a default value
+if test -z "$1"
+	then inputPath="data/words.txt"
+	else inputPath=$1
+fi
 
+#if the outputPath is not passed, uses a default value
+if test -z "$2"
+	then outputPath="data/wordcount"
+	else outputPath=$2
+fi
 
-./activator "run $1 $2"
-sort -nrk2 "data/wordcount/part-r-00000" > sorted.txt
+#run the main class of the project using activator. 
+./activator "run $inputPath $outputPath"
+
+#sort the generated file by value (number of times that the word is present). The original file is sorted by the key (word)
+sort -nrk2 "$outputPath/part-r-00000" > "$outputPath/sorted.txt"
 
