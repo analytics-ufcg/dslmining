dc384ef6e2cd536c70db7c1c78a820829fa0aafa
Andryw Marques Ramos <andryw.neo@gmail.com>
2015-04-06 10:26:51 -0300

    Project examples/DSLMining with problems. Removing to add new project later

--- examples/DSLMining/src/dslmining/main/dsl/RecommendationType.scala
+++
@@ -1,6 +0,0 @@
-package dsl
-
-abstract class RecommendationType (_type:String)
-
-case object USER_BASED_RECOMMENDER extends RecommendationType("USERBASED")
-case object MATRIX_FAC_RECOMMENDER extends RecommendationType("MATRIXFACTORIZATION")
