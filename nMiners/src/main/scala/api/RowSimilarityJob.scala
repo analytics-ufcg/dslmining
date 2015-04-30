8df907596dceb1763d2929b99f9290fd8605a947
Rodolfo Viana <rodolfodelimaviana@gmail.com>
2015-04-30 11:33:27 -0300

    All maps and reduces done for similarity

--- nMiners/src/main/scala/api/RowSimilarityJob.scala
+++ nMiners/src/main/scala/api/RowSimilarityJob.scala
@@ -1,17 +1,21 @@
 package api
 
 import java.io.IOException
-import java.util.Random
+import java.util.{Comparator, Arrays, Random}
 
-import dsl.job.Context
+import com.google.common.base.Preconditions
+import com.google.common.primitives.Ints
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.IntWritable
 import org.apache.hadoop.mapreduce.{Mapper, Reducer}
 import org.apache.mahout.common.{ClassUtils, RandomUtils}
-import org.apache.mahout.math.hadoop.similarity.cooccurrence.{RowSimilarityJob, Vectors}
+import org.apache.mahout.math.Vector.Element
+import org.apache.mahout.math.hadoop.similarity.cooccurrence.{MutableElement, TopElementsQueue, Vectors}
+import org.apache.mahout.math.hadoop.similarity.cooccurrence.measures.VectorSimilarityMeasure
 import org.apache.mahout.math.map.OpenIntIntHashMap
-import org.apache.mahout.math.{RandomAccessSparseVector, Vector, VectorWritable}
+import org.apache.mahout.math.{VectorWritable, RandomAccessSparseVector, Vector}
+import utils.Implicits._
 
 class RowSimilarityJob {
   val NO_THRESHOLD: Double = Double.MinValue
@@ -80,7 +84,7 @@ class RowSimilarityJob {
       val downsampledRow: Vector = rowVector.like
       var usedObservations: Long = 0
       var neglectedObservations: Long = 0
-      import scala.collection.JavaConversions._
+      //      import scala.collection.JavaConversions._
       for (elem <- rowVector.nonZeroes) {
         val columnCount: Int = observationsPerColumn.get(elem.index)
         val columnSampleRate: Double = Math.min(maxObservationsPerColumn, columnCount).toDouble / columnCount.toDouble
@@ -92,7 +96,7 @@ class RowSimilarityJob {
           neglectedObservations += 1
         }
       }
-   /*   ctx.getCounter(Counters.USED_OBSERVATIONS).increment(usedObservations)
+      /*   ctx.getCounter(Counters.USED_OBSERVATIONS).increment(usedObservations)
       ctx.getCounter(Counters.NEGLECTED_OBSERVATIONS).increment(neglectedObservations)*/
       return downsampledRow
     }
@@ -104,7 +108,7 @@ class RowSimilarityJob {
       val rowVector: Vector = similarity.normalize(sampledRowVector)
       var numNonZeroEntries: Int = 0
       var maxValue: Double = Double.MinValue
-      import scala.collection.JavaConversions._
+      //      import scala.collection.JavaConversions._
       for (element <- rowVector.nonZeroes) {
         val partialColumnVector: RandomAccessSparseVector = new RandomAccessSparseVector(Integer.MAX_VALUE)
         partialColumnVector.setQuick(row.get, element.get)
@@ -119,7 +123,7 @@ class RowSimilarityJob {
         maxValues.setQuick(row.get, maxValue)
       }
       norms.setQuick(row.get, similarity.norm(rowVector))
-//      ctx.getCounter(Counters.ROWS).increment(1)
+      //      ctx.getCounter(Counters.ROWS).increment(1)
     }
 
     @throws(classOf[IOException])
@@ -131,35 +135,196 @@ class RowSimilarityJob {
     }
   }
 
- class MergeVectorsReducer extends Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable] {
-    var normsPath :Path
-    var numNonZeroEntriesPath :Path
-    var maxValuesPath :Path
 
-    def MergeVectorsReducer() {
+  class MergeVectorsReducer extends Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable] {
+    var normsPath: Path = null
+    var numNonZeroEntriesPath: Path = null
+    var maxValuesPath: Path = null
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def setup(ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      normsPath = new Path(ctx.getConfiguration.get(NORMS_PATH))
+      numNonZeroEntriesPath = new Path(ctx.getConfiguration.get(NUM_NON_ZERO_ENTRIES_PATH))
+      maxValuesPath = new Path(ctx.getConfiguration.get(MAXVALUES_PATH))
     }
 
-   @throws(classOf[IOException])
-   @throws(classOf[InterruptedException])
-   override def setup(ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
-     val conf: Configuration = ctx.getConfiguration
-     normsPath = new Path(conf.get(NORMS_PATH))
-     numNonZeroEntriesPath = new Path(conf.get(NUM_NON_ZERO_ENTRIES_PATH))
-     maxValuesPath = new Path(conf.get(MAXVALUES_PATH))
-    }
-
-    protected reduce(IntWritable row, Iterable<VectorWritable> partialVectors, Context ctx) throws IOException, InterruptedException {
-      Vector partialVector = Vectors.merge(partialVectors);
-      if(row.get() == -2147483648) {
-        Vectors.write(partialVector, this.normsPath, ctx.getConfiguration());
-      } else if(row.get() == -2147483647) {
-        Vectors.write(partialVector, this.maxValuesPath, ctx.getConfiguration());
-      } else if(row.get() == -2147483646) {
-        Vectors.write(partialVector, this.numNonZeroEntriesPath, ctx.getConfiguration(), true);
-      } else {
-        ctx.write(row, new VectorWritable(partialVector));
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def reduce(row: IntWritable, partialVectors: java.lang.Iterable[VectorWritable], ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      val partialVector: Vector = Vectors.merge(partialVectors)
+      if (row.get == NORM_VECTOR_MARKER) {
+        Vectors.write(partialVector, normsPath, ctx.getConfiguration)
       }
-
+      else if (row.get == MAXVALUE_VECTOR_MARKER) {
+        Vectors.write(partialVector, maxValuesPath, ctx.getConfiguration)
+      }
+      else if (row.get == NUM_NON_ZERO_ENTRIES_VECTOR_MARKER) {
+        Vectors.write(partialVector, numNonZeroEntriesPath, ctx.getConfiguration, true)
+      }
+      else {
+        ctx.write(row, new VectorWritable(partialVector))
+      }
     }
   }
+
+  object CooccurrencesMapper {
+    val BY_INDEX: Comparator[Vector.Element] = new Comparator[Element] {
+      override def compare(one: Element, two: Element): Int = {
+        return Ints.compare(one.index, two.index)
+      }
+    }
+
+    class CooccurrencesMapper extends Mapper[IntWritable, VectorWritable, IntWritable, VectorWritable] {
+      var similarity: VectorSimilarityMeasure = null
+      var numNonZeroEntries: OpenIntIntHashMap = null
+      var maxValues: Vector = null
+      var threshold: Double = .0
+
+      @throws(classOf[IOException])
+      @throws(classOf[InterruptedException])
+      protected override def setup(ctx: Mapper[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+        similarity = ClassUtils.instantiateAs(ctx.getConfiguration.get(SIMILARITY_CLASSNAME), classOf[VectorSimilarityMeasure])
+        numNonZeroEntries = Vectors.readAsIntMap(new Path(ctx.getConfiguration.get(NUM_NON_ZERO_ENTRIES_PATH)), ctx.getConfiguration)
+        maxValues = Vectors.read(new Path(ctx.getConfiguration.get(MAXVALUES_PATH)), ctx.getConfiguration)
+        threshold = ctx.getConfiguration.get(THRESHOLD).toDouble
+      }
+
+      private def consider(occurrenceA: Vector.Element, occurrenceB: Vector.Element): Boolean = {
+        val numNonZeroEntriesA: Int = numNonZeroEntries.get(occurrenceA.index)
+        val numNonZeroEntriesB: Int = numNonZeroEntries.get(occurrenceB.index)
+        val maxValueA: Double = maxValues.get(occurrenceA.index)
+        val maxValueB: Double = maxValues.get(occurrenceB.index)
+        return similarity.consider(numNonZeroEntriesA, numNonZeroEntriesB, maxValueA, maxValueB, threshold)
+      }
+
+      @throws(classOf[IOException])
+      @throws(classOf[InterruptedException])
+      protected override def map(column: IntWritable, occurrenceVector: VectorWritable, ctx: Mapper[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+        val occurrences: Array[Vector.Element] = Vectors.toArray(occurrenceVector)
+        Arrays.sort(occurrences, CooccurrencesMapper.BY_INDEX)
+        var cooccurrences: Int = 0
+        var prunedCooccurrences: Int = 0
+        for (n: Int <- 0 to occurrences.length) {
+          print(n)
+          val occurrenceA: Vector.Element = occurrences(n);
+          val dots: Vector = new RandomAccessSparseVector(Integer.MAX_VALUE);
+          for (m: Int <- n to occurrences.length) {
+            val occurrenceB:Vector.Element = occurrences(m);
+            if (threshold == NO_THRESHOLD || consider(occurrenceA, occurrenceB)) {
+              dots.setQuick(occurrenceB.index(), similarity.aggregate(occurrenceA.get(), occurrenceB.get()));
+              cooccurrences = cooccurrences+1;
+            } else {
+              prunedCooccurrences = prunedCooccurrences+1;
+            }
+          }
+          ctx.write(new IntWritable(occurrenceA.index()), new VectorWritable(dots));
+        }
+        /*ctx.getCounter(Counters.COOCCURRENCES).increment(cooccurrences)
+        ctx.getCounter(Counters.PRUNED_COOCCURRENCES).increment(prunedCooccurrences)*/
+      }
+    }
+
+  }
+
+  class SimilarityReducer extends Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable] {
+    private var similarity: VectorSimilarityMeasure = null
+    private var numberOfColumns: Int = 0
+    private var excludeSelfSimilarity: Boolean = false
+    private var norms: Vector = null
+    private var treshold: Double = .0
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def setup(ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      similarity = ClassUtils.instantiateAs(ctx.getConfiguration.get(SIMILARITY_CLASSNAME), classOf[VectorSimilarityMeasure])
+      numberOfColumns = ctx.getConfiguration.getInt(NUMBER_OF_COLUMNS, -1)
+      Preconditions.checkArgument(numberOfColumns > 0, "Number of columns must be greater then 0! But numberOfColumns = " + numberOfColumns)
+      excludeSelfSimilarity = ctx.getConfiguration.getBoolean(EXCLUDE_SELF_SIMILARITY, false)
+      norms = Vectors.read(new Path(ctx.getConfiguration.get(NORMS_PATH)), ctx.getConfiguration)
+      treshold = ctx.getConfiguration.get(THRESHOLD).toDouble
+    }
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    override def reduce(row: IntWritable, partialDots: java.lang.Iterable[VectorWritable], ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      val partialDotsIterator: Iterator[VectorWritable] = partialDots.iterator
+      val dots: Vector = partialDotsIterator.next.get
+      while (partialDotsIterator.hasNext) {
+        val toAdd: Vector = partialDotsIterator.next.get
+        for (nonZeroElement <- toAdd.nonZeroes) {
+          dots.setQuick(nonZeroElement.index, dots.getQuick(nonZeroElement.index) + nonZeroElement.get)
+        }
+      }
+      val similarities: Vector = dots.like
+      val normA: Double = norms.getQuick(row.get)
+      for (b <- dots.nonZeroes) {
+        val similarityValue: Double = similarity.similarity(b.get, normA, norms.getQuick(b.index), numberOfColumns)
+        if (similarityValue >= treshold) {
+          similarities.set(b.index, similarityValue)
+        }
+      }
+      if (excludeSelfSimilarity) {
+        similarities.setQuick(row.get, 0)
+      }
+      ctx.write(row, new VectorWritable(similarities))
+    }
+  }
+
+  class UnsymmetrifyMapper extends Mapper[IntWritable, VectorWritable, IntWritable, VectorWritable] {
+    private var maxSimilaritiesPerRow: Int = 0
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def setup(ctx: Mapper[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      maxSimilaritiesPerRow = ctx.getConfiguration.getInt(MAX_SIMILARITIES_PER_ROW, 0)
+      Preconditions.checkArgument(maxSimilaritiesPerRow > 0, "Maximum number of similarities per row must be greater then 0!")
+    }
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def map(row: IntWritable, similaritiesWritable: VectorWritable, ctx: Mapper[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      val similarities: Vector = similaritiesWritable.get
+      val transposedPartial: Vector = new RandomAccessSparseVector(similarities.size, 1)
+      val topKQueue: TopElementsQueue = new TopElementsQueue(maxSimilaritiesPerRow)
+      for (nonZeroElement <- similarities.nonZeroes) {
+        val top: MutableElement = topKQueue.top
+        val candidateValue: Double = nonZeroElement.get
+        if (candidateValue > top.get) {
+          top.setIndex(nonZeroElement.index)
+          top.set(candidateValue)
+          topKQueue.updateTop
+        }
+        transposedPartial.setQuick(row.get, candidateValue)
+        ctx.write(new IntWritable(nonZeroElement.index), new VectorWritable(transposedPartial))
+        transposedPartial.setQuick(row.get, 0.0)
+      }
+      val topKSimilarities: Vector = new RandomAccessSparseVector(similarities.size, maxSimilaritiesPerRow)
+      import scala.collection.JavaConversions._
+      for (topKSimilarity <- topKQueue.getTopElements) {
+        topKSimilarities.setQuick(topKSimilarity.index, topKSimilarity.get)
+      }
+      ctx.write(row, new VectorWritable(topKSimilarities))
+    }
+  }
+
+  class MergeToTopKSimilaritiesReducer extends Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable] {
+    private var maxSimilaritiesPerRow: Int = 0
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def setup(ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      maxSimilaritiesPerRow = ctx.getConfiguration.getInt(MAX_SIMILARITIES_PER_ROW, 0)
+      Preconditions.checkArgument(maxSimilaritiesPerRow > 0, "Maximum number of similarities per row must be greater then 0!")
+    }
+
+    @throws(classOf[IOException])
+    @throws(classOf[InterruptedException])
+    protected override def reduce(row: IntWritable, partials:  java.lang.Iterable[VectorWritable], ctx: Reducer[IntWritable, VectorWritable, IntWritable, VectorWritable]#Context) {
+      val allSimilarities: Vector = Vectors.merge(partials)
+      val topKSimilarities: Vector = Vectors.topKElements(maxSimilaritiesPerRow, allSimilarities)
+      ctx.write(row, new VectorWritable(topKSimilarities))
+    }
+  }
+  
 }
