cea1fa1ba02e9d31278b32878378db8b227c0d51
andryw <hatitorluvit000>
2015-05-11 10:18:52 -0300

    1 - Removing commented code in RowSimilarityJobAnalytics class. 2 - The class RowSimilarityJobAnalytics was renamed to SimilarityMatrix

--- nMiners/src/test/scala/nMinersTest/SimlarityMatrixTest.scala
+++ nMiners/src/test/scala/nMinersTest/SimlarityMatrixTest.scala
@@ -13,30 +13,34 @@ class SimlarityMatrixTest extends FlatSpec with Matchers{
 
   val BASE_PHATH = "src/test/resources/"
   val BASE_PHATH_OUTPUT = BASE_PHATH + "output_similarity1/"
+  val BASE_PHATH_SIM_OUTPUT = BASE_PHATH_OUTPUT + "matrix/"
 
   "Similarity_Matrix" should "calculate similarity_matrix" in {
     val inputPath = BASE_PHATH+"data_2/input_test_level1.txt"
     val uservector = BASE_PHATH_OUTPUT+"user_vector/";
     val uservectorFile = uservector + "part-r-00000";
-    val ratingMatrix = BASE_PHATH_OUTPUT+"rating_matrix/";
-    val similarityFile = BASE_PHATH_OUTPUT+"rating_matrix/"+ "part-r-00000";
-    val pathOutputMatrix = BASE_PHATH_OUTPUT + "/data_prepare"
+    val ratingMatrix = BASE_PHATH_SIM_OUTPUT+"rating_matrix/";
+    val similarity = BASE_PHATH_SIM_OUTPUT+"similarity_matrix/";
+
+    val similarityFile = similarity + "part-r-00000";
+    val pathOutputMatrix = BASE_PHATH_SIM_OUTPUT + "/data_prepare"
 
     UserVectorGenerator.runJob(inputPath,uservector, classOf[TextInputFormat],
       classOf[SequenceFileOutputFormat[VarLongWritable, VectorWritable]],true,None)
 
-    RowSimilarityJobAnalytics.runJob(
+    SimilarityMatrix.generateSimilarityMatrix(
       uservectorFile,
-      ratingMatrix,
+      similarity,
       classOf[SequenceFileInputFormat[VarLongWritable,VectorWritable]],
-      classOf[SequenceFileOutputFormat[IntWritable,VectorWritable]],true,similarityClassnameArg = "SIMILARITY_COOCCURRENCE",basePath = BASE_PHATH_OUTPUT, numReduceTasks = None)
+      classOf[SequenceFileOutputFormat[IntWritable,VectorWritable]],true,similarityClassnameArg = "SIMILARITY_COOCCURRENCE",basePath = BASE_PHATH_SIM_OUTPUT, numReduceTasks = None)
 
     PrepareMatrixGenerator.runJob(inputPath1 = similarityFile, inputPath2 = uservectorFile, outPutPath = pathOutputMatrix,
       inputFormatClass = classOf[SequenceFileInputFormat[VarIntWritable, VectorWritable]],
       outputFormatClass = classOf[SequenceFileOutputFormat[VarIntWritable, VectorAndPrefsWritable]],
       deleteFolder = true)
 
-   // MultiplyMatrix.run(inputPath = pathOutputMatrix, outputPath = BASE_PHATH_OUTPUT)
+
+
   }
 
   //TODO TESTS TO COMPARE MAHOUT OUTPUT AND nMINERS OUTPUT
