e864c9f5ec91bd789e3b9b0607e866a6cc57c598
Rodolfo Viana <rodolfodelimaviana@gmail.com>
2015-07-14 09:20:26 -0300

    Spark item similarity script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,6 @@
 
 $SPARK_HOME/sbin/start-all.sh
 
-bin/mahout spark-itemsimilarity --input $2 --output $3 --master spark://$1:7077 --sparkExecutorMem 6g
+bin/mahout spark-itemsimilarity --master spark://$1:7077 --input $2 --output $3 --sparkExecutorMem 6g
 
 $SPARK_HOME/sbin/stop-all.sh
