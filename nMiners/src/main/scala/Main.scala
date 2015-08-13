111bb4349534eee35dddfc239531667d49818662
arthur <arthur.senaufcg@gmail.com>
2015-08-13 17:25:27 -0300

    Merging

--- nMiners/src/main/scala/Main.scala
+++ nMiners/src/main/scala/Main.scala
@@ -6,16 +6,26 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-//    val dataset = "data/input.dat"
-//    val output = "data/output"
+    val dataset = "/home/arthur/dslmining_Temp/dslmining/nMiners/data/input.dat"
+    val output = "data/out"
 
-    val dataset = args(0)
-    val output = args(1)
+
+
+//    val dataset = args(0)
+//    val output = args(1)
 
     parse_data on dataset then
       produce(user_vectors as "user_vector") then
-      produce(similarity_matrix) write_on output then
-      multiply("coocurrence" by "user_vector") then
+      produce(similarity_matrix as "coocurrence") then execute
+      multiply("user_vector" by "coocurrence") then
       produce(recommendation) write_on output then execute
+
+
+//    parse_data on dataset then
+//      dsl_spark.job.JobUtils.produce(user_vectors as "matrix1") then
+//      dsl_spark.job.JobUtils.produce(similarity_matrix as "matrix2")  then
+//      multiply("matrix1" by "matrix2") then
+//      produce(recommendation) write_on output then dsl_spark.job.execute
+
   }
 }
