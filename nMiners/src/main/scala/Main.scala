2ec1e582164e5ab295b775c4832c3050ab1ea25b
andryw <hatitorluvit000>
2015-08-13 17:24:38 -0300

    Spark ok. Hadoop with problem

--- nMiners/src/main/scala/Main.scala
+++ nMiners/src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import com.typesafe.config.ConfigFactory
 import dsl_spark.job.JobUtils._
 import dsl_spark.job._
 import dsl_spark.job.Implicits._
@@ -8,14 +9,15 @@ object Main {
 
 //    val dataset = "data/input.dat"
 //    val output = "data/output"
+val config = ConfigFactory.load()
 
-    val dataset = args(0)
-    val output = args(1)
+    val dataset = config.getString("nMiners.in")
+    val output = "src/main/resources/output2.dat"
 
     parse_data on dataset then
       produce(user_vectors as "user_vector") then
       produce(similarity_matrix) write_on output then
-      multiply("coocurrence" by "user_vector") then
+      multiply("similarity_matrix" by "user_vector") then
       produce(recommendation) write_on output then execute
   }
 }
