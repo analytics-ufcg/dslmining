4ab400a52af38953e19b71a200ccb9f3441491f0
arthur <arthur.senaufcg@gmail.com>
2015-08-14 10:22:06 -0300

    Deleting output from code

--- nMiners/src/main/scala/Main.scala
+++ nMiners/src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import com.typesafe.config.ConfigFactory
 import dsl_spark.job.JobUtils._
 import dsl_spark.job._
 import dsl_spark.job.Implicits._
@@ -6,16 +7,16 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-//    val dataset = "data/input.dat"
-//    val output = "data/output"
+    val dataset = "data/input.dat"
+    val output = "data/output"
 val config = ConfigFactory.load()
 
-    val dataset = config.getString("nMiners.in")
-    val output = "src/main/resources/output2.dat"
+//    val dataset = config.getString("nMiners.in")
+//    val output = config.getString("nMiners.out")
 
     parse_data on dataset then
       produce(user_vectors as "user_vector") then
-      produce(similarity_matrix) write_on output then
+      produce(similarity_matrix)  then
       multiply("similarity_matrix" by "user_vector") then
       produce(recommendation) write_on output then execute
   }
