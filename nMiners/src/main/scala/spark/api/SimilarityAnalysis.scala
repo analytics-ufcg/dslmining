176122bff90e3da6ab5952b3cfcdf379bec6f589
Lucas Bonatto Miguel <lucas.bonatto@b2wdigital.com>
2015-12-06 13:05:29 -0200

    t

--- nMiners/src/main/scala/spark/api/SimilarityAnalysis.scala
+++ nMiners/src/main/scala/spark/api/SimilarityAnalysis.scala
@@ -70,7 +70,7 @@ object SimilarityAnalysis extends Serializable {
    *         cross-cooccurrence
    */
   def cooccurrences(drmARaw: DrmLike[Int], randomSeed: Int = 0xdeadbeef, maxInterestingItemsPerThing: Int = 50,
-                    maxNumInteractions: Int = 500, drmBs: Array[DrmLike[Int]] = Array()): List[DrmLike[Int]] = {
+                    maxNumInteractions: Int = 500, drmBs: Array[DrmLike[Int]] = Array()): List[DrmLike[Nothing]] = {
 
     implicit val distributedContext = drmARaw.context
 
@@ -150,7 +150,7 @@ object SimilarityAnalysis extends Serializable {
                         randomSeed: Int = 0xdeadbeef,
                         maxInterestingItemsPerThing: Int = 50,
                         maxNumInteractions: Int = 500):
-  List[DrmLike[Int]] = {
+  List[DrmLike[Nothing]] = {
     val primaryDrm = drms(0)
     val secondaryDrms = drms.drop(1)
     cooccurrences(primaryDrm, randomSeed, maxInterestingItemsPerThing,
